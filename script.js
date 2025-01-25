7799b8966c4a64af66453eac78162bef026dc9c9
salman-itShaala <salman.shaikh@itshaala.com>
2025-01-25 17:06:14 +0530

    completed js part

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hii");
+const skillsContainer = document.getElementById("skills-section");
+const projectsSection = document.getElementById("projects-section");
+
+function showProjects() {
+  // hide skills and show projects
+  skillsContainer.classList.remove("active");
+  skillsContainer.classList.add("inactive");
+  projectsSection.classList.add("active");
+  projectsSection.classList.remove("inactive");
+}
+
+function showSkills() {
+  // hide projects and show skills
+  projectsSection.classList.remove("active");
+  projectsSection.classList.add("inactive");
+  skillsContainer.classList.remove("inactive");
+  skillsContainer.classList.add("active");
+}
